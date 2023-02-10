b674f4288f29928d8b9bd68eec70c543ff50bb60
Paynnn <123642649+Paynnn@users.noreply.github.com>
2023-02-10 15:43:07 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -12,7 +12,7 @@
 //console.log(document.forms);
 //console.log(document.forms[0]);
 
-console.log(document.getElementsByClassName("list-item"));
+//console.log(document.getElementsByClassName("list-item"));
 
 let items=document.getElementsByClassName("list-item");
 
@@ -22,3 +22,39 @@ for(let i=0;i<items.length;i++){
     if(i%2==0)items[i].style.backgroundColor ="cyan";
     else items[i].style.backgroundColor = "lightgrey"
 }
+//console.log(document.getElementById("header-title"))
+let headerTitle = document.getElementById("header-title");
+
+headerTitle.innerHTML="<h3> ASD </h3>"
+console.log(headerTitle.textContent)
+console.log(headerTitle.innerText)
+
+//textContent vs innerText
+
+console.log(document.getElementsByTagName("li"));
+
+let li = document.getElementsByTagName("li");
+
+li[1].textContent = "asd";
+li[1].style.fontSize = "20px";
+li[1].fontWeight = "bold";
+
+let title= document.getElementsByClassName("title")
+title[1].textContent = "Promenio title"
+
+let form = document.getElementsByClassName("form-inline");
+form[0].style.backgroundColor = "yellow"
+
+let container = document.getElementById("container");
+container.style.backgroundColor = "Green"
+
+let drugiLink=document.querySelector("#google a")
+console.log(drugiLink)
+
+drugiLink.style.backgroundColor = "red"
+
+document.querySelector("#asd").style.backgroundColor ="cyan";
+
+
+
+
